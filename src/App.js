0a64f3a6fec9d81755fcf549eb8e05442a5cbe09
Pravin <pravinkumar2108@gmail.com>
2024-08-27 01:20:54 +0530

    item list

---
+++ src/App.js
@@ -0,0 +1,59 @@
+const App = () => {
+  return (
+  <div className="categories-container">
+    <div className="category-container">
+      {/* img */}
+      <div className="category-body-container">
+      <h2>Hats</h2>
+      <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* img */}
+      <div className="category-body-container">
+        <h2>Hoodies</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>                               
+    <div className="category-container">
+      {/* img */}
+      <div className="category-body-container">
+        <h2>Air Jordans</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+        {/* img */}
+        <div className="category-body-container">
+          <h2>Leather</h2>
+          <p>Shop Now</p>
+        </div>
+    </div>
+      <div className="category-container">
+        {/* img */}
+        <div className="category-body-container">
+          <h2>Suite</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* img */}
+        <div className="category-body-container">
+          <h2>Women's</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* img */}
+        <div className="category-body-container">
+          <h2>Men's</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+  </div>
+
+  )
+}
+export default App;

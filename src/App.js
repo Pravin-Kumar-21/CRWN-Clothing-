91730c3d6eb257321c305452c0a6f921a20ce8f1
Pravin <pravinkumar2108@gmail.com>
2024-09-01 01:28:57 +0530

    category-item container

--- src/App.js
+++ src/App.js
@@ -1,52 +1,55 @@
 import './categories.styles.scss';
-
+import CategoryItem from './components/category-item/CategoryItem';
 const App = () => {
   const categories = [
     {
       id: 1,
-      item: "Hats",
-      Imgurl: "https://i.ibb.co/cvpntL1/hats.png"
+      item: "Casuals",
+      Imgurl: "https://i.ibb.co/ZhsJFYr/Casual.jpg"
     },
     {
       id: 2,
-      item: "Sneakers",
-      Imgurl: "https://i.ibb.co/0jqHpnp/sneakers.png"
+      item: "Party",
+      Imgurl: "https://i.ibb.co/KF8352b/Fashion-For-Women-Official-Online-Store.jpg"
     },
     {
       id: 3,
-      item: "Jackets",
-      Imgurl: "https://i.ibb.co/px2tCc3/jackets.png"
+      item: "Leather",
+      Imgurl: "https://i.ibb.co/B4L2K27/Jacket.png"
     },
     {
       id: 4,
       item: "Mens",
-      Imgurl: "https://i.ibb.co/R70vBrQ/men.png"
+      Imgurl: "https://i.ibb.co/kSk88rp/Casuals.jpg"
     },
     {
       id: 5,
       item: "Womens",
-      Imgurl: "https://i.ibb.co/GCCdy8t/womens.png"
+      Imgurl: "https://i.ibb.co/k5BQs75/pose.jpg"
+    },
+    {
+      id: 6,
+      item: "Suite",
+      Imgurl: "https://i.ibb.co/jGcs2sn/Suite.jpg"
+    },
+    {
+      id: 7,
+      item: "Ethnic",
+      Imgurl: "https://i.ibb.co/RC05YFY/Ethnic.jpg"
+    },
+    {
+      id: 8,
+      item: "Exotic",
+      Imgurl: "https://i.ibb.co/Npc9S70/Exotic.png"
     },
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map(({ id, item, Imgurl }) => (
-        <div className="category-container" key={id}>
-           <div
-            className="background-image"
-            style={{
-              backgroundImage: `url(${Imgurl})`,
-            }}
-          />
-          <div className="category-body-container">
-            <h2>{item}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
+    <div className="categories-container">{
+      categories.map(({Imgurl,item,id}) => (
+        <CategoryItem Imgurl ={Imgurl} item ={item} key={id}/>    
+    ))}
     </div>
-  );
+  )
 };
-
 export default App;
---
+++ src/components/category-item/CategoryItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+const CategoryItem =  ({id, item, Imgurl}) =>{
+// console.log(id)
+// console.log(item)
+// console.log(Imgurl)
+return (
+  <div className="category-container" key={id}>
+    <div className="background-image" style={{ backgroundImage: `url(${Imgurl})` }}/>
+    <div className="category-body-container">
+      <h2>{item}</h2>
+      <FontAwesomeIcon icon={faCartShopping} />
+    </div>
+  </div>
+)
+};
+export default CategoryItem;
